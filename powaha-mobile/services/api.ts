67047dae26aed6e64570014fa12a657381a88345
tanuja23370 <tanujanimbakkannavar23@gmail.com>
2026-01-19 10:26:30 +0530

    save

--- powaha-mobile/services/api.ts
+++ powaha-mobile/services/api.ts
@@ -1,4 +1,6 @@
-const BASE_URL = "https://powaha-mobile.onrender.com";
+import { API_BASE_URL } from "../config/api";
+
+const BASE_URL = API_BASE_URL;
 
 export const getUserNotifications = async (userId: number) => {
   try {
